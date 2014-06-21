8739d1c729d5ed60c304cf5a25e552e2f1430b15
Nader Hendawi <nader@backupify.com>
2014-06-21 12:06:20 -0400

    git url
    Signed-off-by: Nader Hendawi <nader@backupify.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'getstrong'
-set :repo_url, 'git@example.com:naderhen/getstrong_api.git'
+set :repo_url, 'git@github.com:naderhen/getstrong_api.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
